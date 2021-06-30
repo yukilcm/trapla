2f9b80c030469af6cabb9a81e21f47b203fa65f6
EC2 Default User <ec2-user@ip-172-31-16-120.ap-northeast-1.compute.internal>
2021-06-30 12:02:39 +0000

    travelsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210625120300) do
+ActiveRecord::Schema.define(version: 20210630115155) do
+
+  create_table "travels", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.string   "name",       null: false
+    t.date     "start_date", null: false
+    t.date     "end_date",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_travels_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
