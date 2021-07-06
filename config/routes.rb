b9ff0e64ccb034969ad816773a852062137f3ed5
EC2 Default User <ec2-user@ip-172-31-16-120.ap-northeast-1.compute.internal>
2021-07-06 15:08:55 +0000

    確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'top#index'
+  root 'travels#index'
+
+  resources :travels
+
+
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
