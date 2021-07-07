d341fa76ca433ef0ff494c8fd7e158580d5c8585
EC2 Default User <ec2-user@ip-172-31-16-120.ap-northeast-1.compute.internal>
2021-07-07 09:03:41 +0000

    リセット

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,7 +1,21 @@
 class TravelsController < ApplicationController
   def index
+    @travels = current_user.travels
   end
 
   def new
+    @travel = current_user.travels.new
+  end
+
+  def create
+    @travel = current_user.travels.new(travel_params)
+    @travel.save!
+    redirect_to root_path
+  end
+
+  private
+
+  def travel_params
+    params.require(:travel).permit(:name, :start_date, :end_date)
   end
 end
