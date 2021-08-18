c378b37b76bd6d1734697d50942bfc27b91d3b4b
EC2 Default User <ec2-user@ip-172-31-16-120.ap-northeast-1.compute.internal>
2021-08-18 10:38:34 +0000

    0818lb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def index
+      @travel = current_user.travels.find(params[:travel_id])
+  end  
 end
