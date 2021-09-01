3c8b5876ee35cbebf421352fb2836a61074cf41a
EC2 Default User <ec2-user@ip-172-31-16-120.ap-northeast-1.compute.internal>
2021-09-01 12:13:10 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_804_121_427) do
-  create_table 'checklists', force: :cascade do |t|
-    t.string   'title',                      null: false
-    t.integer  'travel_id',                  null: false
-    t.boolean  'checked', default: false, null: false
-    t.datetime 'created_at',                 null: false
-    t.datetime 'updated_at',                 null: false
+ActiveRecord::Schema.define(version: 20210804121427) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "checklists", force: :cascade do |t|
+    t.string   "title",                      null: false
+    t.integer  "travel_id",                  null: false
+    t.boolean  "checked",    default: false, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
-  create_table 'schedules', force: :cascade do |t|
-    t.integer  'travel_id'
-    t.datetime 'start_at',   null: false
-    t.datetime 'end_at',     null: false
-    t.string   'title',      null: false
-    t.string   'memo'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer  'cost'
+  create_table "schedules", force: :cascade do |t|
+    t.integer  "travel_id"
+    t.datetime "start_at",   null: false
+    t.datetime "end_at",     null: false
+    t.string   "title",      null: false
+    t.string   "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "cost"
   end
 
-  create_table 'travels', force: :cascade do |t|
-    t.integer  'user_id'
-    t.string   'name'
-    t.date     'start_date'
-    t.date     'end_date'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_travels_on_user_id'
+  create_table "travels", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_travels_on_user_id", using: :btree
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'email',                  default: '', null: false
-    t.string   'encrypted_password',     default: '', null: false
-    t.string   'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
+
 end
